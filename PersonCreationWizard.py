b555af9786cf5bd51810e451084d95d7dec66935
Benjamin Phillips <bdphillips013@gmail.com>
2019-05-24 17:29:41 -0400

    Overrode accept() method

--- PersonCreationWizard.py
+++ PersonCreationWizard.py
@@ -7,3 +7,7 @@ class PersonCreationWizard(QWizard):
     def __init__(self):
         super().__init__()
         super().addPage(AddPersonNamePage())
+
+    def accept(self) -> None:
+        name = super().field("name").toString()
+        dob = super().field("dob").toDate()
